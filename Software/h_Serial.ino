f7b81c966c1e4a5a053cce17ba839ab3ce4b50ef
RIN67630 <lazlo.lebrun@googlemail.com>
2021-07-12 17:50:25 +0200

    Update h_Serial.ino

--- Software/h_Serial.ino
+++ Software/h_Serial.ino
@@ -13,9 +13,9 @@ yield();
         if (serialPeriodicity == '!') serialPage = 0; // One shot reset serial page.
         break;
       case 'D':   // Debug Report
-        // Console1.printf("ADC_VinRaw:%i Vin:%06.3f ADC_IinRaw:%i Iin:%06.3f \nADC_VoutRaw:%i Vout:%06.3f ADC_IoutRaw:%i Iout:%06.3f PWM_SetVout:%04i SetVout:%06.3f PWM_SetIout:%04i SetIout:%06.3f PWM_Fan:%04i \n" , ADC_VinRaw, dashboard.Vin, ADC_IinRaw, dashboard.Iin, ADC_VoutRaw, dashboard.Vout, ADC_IoutRaw, dashboard.Iout, PWM_SetVout, dashboard.SetVout, PWM_SetIout, dashboard.SetIout, PWM_Fan);
+        Console1.printf("ADC_VinRaw:%i Vin:%06.3f ADC_IinRaw:%i Iin:%06.3f \nADC_VoutRaw:%i Vout:%06.3f ADC_IoutRaw:%i Iout:%06.3f PWM_SetVout:%04i SetVout:%06.3f PWM_SetIout:%04i SetIout:%06.3f PWM_Fan:%04i \n" , ADC_VinRaw, dashboard.Vin, ADC_IinRaw, dashboard.Iin, ADC_VoutRaw, dashboard.Vout, ADC_IoutRaw, dashboard.Iout, PWM_SetVout, dashboard.SetVout, PWM_SetIout, dashboard.SetIout, PWM_Fan);
         // Console1.printf("Vin:%06.3f SetVin:%06.3f Iin:%06.3f Vout:%06.3f SetVout:%06.3f Iout:%06.3f SetIout:%06.3f ConIout:%06.3f I_Value %06.3f  CtrlMode:%02i\n" , dashboard.Vin, dashboard.SetVin, dashboard.Iin, dashboard.Vout, dashboard.SetVout, dashboard.Iout, dashboard.SetIout, dashboard.ConIout, I_value, dashboard.CtrlMode );
-        Console1.printf("Vin:%06.3f SetVin:%06.3f ConVin:%06.3f Iout:%06.3f ConIout:%06.3f MPPT_voc:%06.3f  fractionVoc:%06.3f\n" , dashboard.Vin, dashboard.SetVin, dashboard.ConVin, dashboard.Iout, dashboard.ConIout, MPPT_voc,  fractionVoc);
+        // Console1.printf("Vin:%06.3f SetVin:%06.3f ConVin:%06.3f Iout:%06.3f ConIout:%06.3f MPPT_voc:%06.3f  fractionVoc:%06.3f\n" , dashboard.Vin, dashboard.SetVin, dashboard.ConVin, dashboard.Iout, dashboard.ConIout, MPPT_voc,  fractionVoc);
         if (serialPeriodicity == '!') serialPage = 0; // One shot reset serial page.
         break;
       case 'J':   // Job Duration
